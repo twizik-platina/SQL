d0c8f164ec088072a8e08a4551dcc758cb3e0618
twizik-platina <twizyaaa@gmail.com>
2026-06-03 14:41:44 +0300

    Add files via upload

---
+++ sql_dz_1.sql
@@ -0,0 +1,29 @@
+-- INSERT INTO students (first_name, last_name, age, group_name, average_score)
+--VALUES
+  --  ('Иван', 'Петров', 13, 'Python-1', 4.5),
+    --('Анна', 'Смирнова', 14, 'Python-1', 4.8),
+    --('-Олег', 'Иванов', 12, 'Python-2', 3.9),
+   -- ('Мария', 'Кузнецова', 15, 'Python-2', 4.2),
+   -- ('Егор', 'Соколов', 13, 'Python-1', 5.0)
+
+-- SELECT * FROM students
+
+-- SELECT * FROM students WHERE group_name = 'Python-1'
+
+-- SELECT * FROM students WHERE age > 13
+
+-- SELECT * FROM students WHERE average_score > 4.0
+
+-- UPDATE students SET average_score = 4.0 WHERE id = 3
+
+-- SELECT * FROM students
+
+-- UPDATE students SET group_name = 'Python-2' WHERE id = 2
+
+-- SELECT * FROM students
+
+-- DELETE FROM students WHERE id = 4
+
+-- SELECT * FROM students
+
+
